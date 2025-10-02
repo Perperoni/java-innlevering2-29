6a162e85dac86e1a321ee6217a14d1e825da5e11
Martin Hovland Vigdal <martin.vigdal@gmail.com>
2025-10-02 11:11:41 +0200

    Fix formating

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -4,15 +4,47 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
+        int longest = findLongest(matrise);
+        String[][] stringMatrise = buildStringMatrise(matrise, longest);
 
-        for(int i=0;i<matrise.length;i++) {
+        for (int i = 0; i < stringMatrise.length; i++) {
             System.out.print("| ");
-            for(int j=0;j<matrise[i].length;j++) {
-                System.out.print(matrise[i][j]+" ");
+            for (int j = 0; j < stringMatrise[i].length; j++) {
+                System.out.print(stringMatrise[i][j] + " ");
             }
             System.out.println("|");
         }
+        System.out.println("");
 	}
+
+    static int findLongest(int[][] matrise) {
+        int longest = 0;
+
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+                String str = String.valueOf(matrise[i][j]);
+                if (str.length() > longest) longest = str.length();
+            }
+        }
+        return longest;
+    }
+
+    static String[][] buildStringMatrise(int[][] matrise, int longest) {
+        String[][] stringMatrise = new String[matrise.length][];
+
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+                String str = String.valueOf(matrise[i][j]);
+
+                String spacerStr = "";
+                for (int k = 0; k < longest - str.length(); k++) {
+                    spacerStr += " ";
+                }
+                stringMatrise[i][j] = spacerStr + str;
+            }
+        }
+        return stringMatrise;
+    }
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
